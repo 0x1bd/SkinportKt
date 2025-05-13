2ee32cb330e75241504ffb4f9e8db470513abf2c
kvxd <redslyice@outlook.de>
2025-05-13 19:51:09 +0200

    feat: Maven Central deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     kotlin("jvm") version "2.1.20"
     kotlin("plugin.serialization") version "2.1.20"
+    id("io.deepmedia.tools.deployer") version "0.18.0"
 }
 
 group = "io.github.kvxd"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -22,6 +23,37 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+deployer {
+    content {
+        component {
+            fromJava()
+        }
+    }
+
+    projectInfo {
+        description = project.description.toString()
+        url = "https://github.com/0x1bd/SkinportKt"
+        scm.fromGithub("0x1bd", "SkinportKt")
+        license("GNU GPL 3.0", "https://www.gnu.org/licenses/gpl-3.0.txt")
+        developer("0x1bd", "0x1bd@proton.me")
+        groupId = project.group.toString()
+    }
+
+    centralPortalSpec {
+        signing.key = secret("SIGNING_KEY")
+        signing.password = secret("SIGNING_PASSPHRASE")
+
+        auth.user = secret("CENTRAL_USERNAME")
+        auth.password = secret("CENTRAL_PASSWORD")
+    }
 }
